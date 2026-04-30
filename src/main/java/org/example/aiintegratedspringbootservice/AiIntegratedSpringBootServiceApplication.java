4e0abd22d4220486527efac7d5526468afcae574
Gabriela Aguirre <gvaguirres@gmail.com>
2026-04-30 16:49:47 +0200

    Initial commit

---
+++ src/main/java/org/example/aiintegratedspringbootservice/AiIntegratedSpringBootServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.aiintegratedspringbootservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiIntegratedSpringBootServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiIntegratedSpringBootServiceApplication.class, args);
+    }
+
+}
